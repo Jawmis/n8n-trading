0fd47486d8a98f72b1dc857ddec64ff1dc8e753b
Jamis Bade <jamisbade53@gmail.com>
2026-08-18 16:46:23 -0400

    Fixed : Nodes schema fixed in packages/common/types/index.ts , workflow endpoint is working

--- packages/common/types/index.ts
+++ packages/common/types/index.ts
@@ -12,9 +12,9 @@ export const SigninSchema= z.object({
 
 export const CreateWorkflowSchema = z.object({
     nodes: z.array(z.object({
-        type: z.string(),
+        nodeId: z.string(),
         data: z.object({
-            kind: z.enum(["action", "trigger"]),
+            kind: z.enum(["ACTION", "TRIGGER"]),
             metadata : z.any()
         }),
         id: z.string(),
@@ -31,11 +31,16 @@ export const CreateWorkflowSchema = z.object({
 })
 
 export const UpdateWorkflowSchema = z.object({
-    nodes: z.array(z.object({
+   nodes: z.array(z.object({
+        nodeId: z.string(),
+        data: z.object({
+            kind: z.enum(["ACTION", "TRIGGER"]),
+            metadata : z.any()
+        }),
         id: z.string(),
         position: z.object({
-            x : z.number(),
-            y : z.number()
+            x: z.number(),
+            y: z.number()
         })
     })),
     edges: z.array(z.object({
